839dad6fdebcb6a5de3ac687e612e0cfc7e220f3
jiXingLiu0625 <105674423+jiXingLiu0625@users.noreply.github.com>
2022-05-25 22:51:23 -0700

    Add files via upload

---
+++ OOP Udemy/25/largestPrime.java
@@ -0,0 +1,23 @@
+public class largestPrime {
+    public static void main(String[] args) {
+
+    }
+    public static int getLargestPrime(int number) {
+
+        if (number <= 0 || number <= 1) {
+            return -1;
+        }
+        else {
+            int largestPrime = 2;
+            while (largestPrime < number) {
+                if (number % largestPrime != 0) {
+                    largestPrime++;
+                } else {
+                    number = number / largestPrime;
+
+                }
+            }
+            return number;
+        }
+    }
+}
