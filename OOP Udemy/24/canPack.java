5af977cf5b94530eac70b43a484cafa769d93bb6
jiXingLiu0625 <105674423+jiXingLiu0625@users.noreply.github.com>
2022-06-03 21:47:33 -0700

    Add files via upload

---
+++ OOP Udemy/24/canPack.java
@@ -0,0 +1,20 @@
+public class canPack {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean canPack(int bigCount, int smallCount, int goal) {
+        if (bigCount < 0 || smallCount < 0 || goal < 0) {
+            return false;
+        }
+        if(goal > ((bigCount * 5) + smallCount)) {
+            return false;
+        }
+
+        if((goal % 5) > smallCount) {
+            return false;
+        }
+        return true;
+    }
+}
+
